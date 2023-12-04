056517c1a85cfc042afc3d975d7c1e99f89e1a9f
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-04 11:25:08 +0100

    Solution 1

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button > Press the Button </Button>
+      <Button > Try the Button </Button>
+      <Button > Click here </Button>
+      <Button > Button </Button>
     </main>
   );
 }
 
-function Button() {
+function Button({children}) {
   return (
     <button className="button" type="button">
-      Click me!
+     ({children})
     </button>
   );
 }
